548942d546d038ccc1dd7e8e287a04b6f7bc8739
CDevv <cdevyt@gmail.com>
2025-09-01 19:12:05 +0300

    Add HTTP express.js server + endpoint for submitting a niko which sends discord msg and dm

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ dotenv.config();
 const fs = require('node:fs')
 const path = require('node:path')
 
-const { Client, Events, GatewayIntentBits, MessageFlags, Collection } = require('discord.js')
+const { Client, Events, GatewayIntentBits, MessageFlags, Collection, EmbedBuilder, AttachmentBuilder } = require('discord.js')
 const token = process.env.DISCORD_TOKEN
+const express = require('express')
+const cors = require('cors')
+const formidable = require('express-formidable');
+
+const app = express()
+app.use(cors())
+app.use(formidable())
+const port = 3000
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
@@ -39,6 +47,60 @@ client.on(Events.InteractionCreate, async interaction => {
     } catch (error) {
         console.error(error);
     }
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/msg', (req, res) => {
+    client.channels.cache.get('1408078874213875726')
+    .send('If you are seeing this then i successfully sent a msg because of a POST to localhost:3000/msg');
+    res.send('Sent message to discord channel');
+})
+
+app.post('/upload', (req, res) => {
+    const abilities = req.fields['abilities'].split('|||');
+    console.log(abilities);
+
+    let abilitiesFields = []
+    for (const element of abilities) {
+        abilitiesFields.push({ name: 'Ability', value: element })
+    }
+
+    const originalFileName = req.files['files[0]'].name;
+
+    try {
+        let file = new AttachmentBuilder(req.files['files[0]'].path);
+        file.setName(originalFileName)
+        let embed = new EmbedBuilder()
+            .setTitle(`Submission: ${req.fields['name']}`)
+            .setDescription(`${req.fields['description']}`)
+            .addFields({ name: 'Author', value: `${req.fields['author']}` })
+            .addFields({ name: 'Full Description', value: `${req.fields['full_desc']}` })
+            .setImage(`attachment://${originalFileName}`)
+
+        embed = embed.addFields(abilitiesFields);
+
+        client.channels.cache.get(process.env['SUBMISSIONS_CHANNEL'])
+        .send({ embeds: [embed], files: [file] })
+
+        client.users.fetch(req.fields['author_id'])
+        .then(async u => {
+            await u.createDM(true)
+            await u.dmChannel.send(`Your nikosona submission was sent: ${req.fields['name']}`)
+        })
+        .catch(e => console.log(e))
+    } catch (error) {
+        console.log(error)
+        res.status(400).send(JSON.stringify({ msg: `Error! ${error}` }))
+    }
+
+    res.send(JSON.stringify(req.fields));
+})
+
+app.listen(port, () => {
+  console.log(`[Nikodex2-bot] Bot's server listening on port ${port}`)
 })
 
 client.login(token)
