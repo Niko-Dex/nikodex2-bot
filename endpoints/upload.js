f04acea41d3f731adbfcf263063b4f6c3dd710ba
D. Quan <60545346+sudoker0@users.noreply.github.com>
2025-09-10 18:04:02 +0700

    added validation for max length/size for submit entry

--- endpoints/upload.js
+++ endpoints/upload.js
@@ -1,16 +1,85 @@
+const sharp = require('sharp');
 const { getDiscordUser } = require('../helper');
-const { EmbedBuilder, AttachmentBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js')
+const { EmbedBuilder, AttachmentBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Client} = require('discord.js')
 const adminIdList = process.env.ADMIN_ID_LIST.split(';')
+
+/**
+ * @param {string} str
+ * @returns {number}
+ */
+function getStrLenInBytes(str) {
+    return new TextEncoder().encode(str).length
+}
+
+/**
+ * 
+ * @param {import("express").Request} req 
+ */
+async function validateSubmit(req) {
+    // validate name (max 120 bytes)
+    if (getStrLenInBytes(req.fields["name"]) > 120) {
+        return "Length for name exceeded 120 bytes!"
+    }
+
+    // validate short description (max 250 bytes)
+    if (getStrLenInBytes(req.fields["description"]) > 250) {
+        return "Length for short description exceeded 250 bytes!"
+    }
+
+    // validate description (max 1000 bytes)
+    if (getStrLenInBytes(req.fields["full_desc"]) > 1000) {
+        return "Length for description exceeded 1000 bytes!"
+    }
+
+    // validate abilities (max 20 abilities, max 500 bytes/ASCII characters across all abilities)
+    const abilities = req.fields["abilities"].split("|||")
+    if (abilities.length > 20) {
+        return "Number of abilities exceeded 20 abilities!"
+    }
+
+    let abilitiesCnt = 0
+    for (let i of abilities) {
+        abilitiesCnt += getStrLenInBytes(i)
+    }
+    if (abilitiesCnt > 500) {
+        return "Total length for all abilities exceeded 500 bytes!"
+    }
+
+    // validate image (max 2MB, max dimension of 1024x1024)
+    const imageFile = req.files['files[0]']
+    if (imageFile.size > 2 * 1024 * 1024) {
+        return "Size for image exceeded 2MB!"
+    }
+
+    const imageMetadata = await sharp(imageFile.path).metadata()
+    if (imageMetadata.width > 1024 || imageMetadata.height > 1024) {
+        return "Dimension for image exceeded 1024x1024!"
+    }
+    return null
+}
+
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @param {Client} client 
+ */
 
 async function upload(req, res, client) {
     const token = req.headers.authorization ?? ""
     const user = await getDiscordUser(token)
     if (!user) {
         res.status(401).send(JSON.stringify({ detail: `Unauthenticated!` }))
+        return
+    }
+
+    const errMsg = await validateSubmit(req)
+    if (errMsg) {
+        res.status(400).send(JSON.stringify({ detail: errMsg }))
+        return
     }
 
     const abilities = req.fields['abilities'].split('|||');
-    console.log(abilities);
 
     let abilitiesFields = []
     if (req.fields['abilities']) {
@@ -26,9 +95,9 @@ async function upload(req, res, client) {
         file.setName(originalFileName)
         let embed = new EmbedBuilder()
             .setTitle(`Submission: ${req.fields['name']}`)
-            .setDescription(`${req.fields['description']}`)
+            .setDescription(req.fields['full_desc'])
             .addFields({ name: 'Author', value: `${user['username']} (${user['id']})` })
-            .addFields({ name: 'Full Description', value: `${req.fields['full_desc']}` })
+            .addFields({ name: 'Short Description', value: `${req.fields['description']}` })
             .setImage(`attachment://${originalFileName}`)
 
         embed = embed.addFields(abilitiesFields);
@@ -83,7 +152,7 @@ async function upload(req, res, client) {
         res.status(400).send(JSON.stringify({ detail: `Error! ${error}` }))
     }
 
-    res.send(JSON.stringify(req.fields));
+    res.status(200).send(JSON.stringify(req.fields));
 }
 
 module.exports = {
