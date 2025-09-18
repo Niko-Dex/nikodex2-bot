5e20bfeb970a2f8f68756b0c42988208ba30b0e1
ziolkoplays <201670758+ziolkoplayscode@users.noreply.github.com>
2025-09-18 16:37:06 -0400

    Added getniko command

---
+++ commands/get_niko.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const apiUri = process.env.API_URI
+const Buffer = require('buffer').Buffer
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('getniko')
+        .setDescription('Replies with details of that Nikosona!')
+        .addStringOption(o => o
+                .setName("niko")
+                .setDescription("The name of the niko you want to get.")
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const niko = interaction.options.getString("user_id");
+        await interaction.reply("Let me try to find them..");
+        try {
+            const res = await fetch(`${apiUri}/nikos/name?${niko}`);
+            const resJson = await res.json();
+            if (res.status > 299) {
+                await interaction.editReply(`Result is not OK: HTTP ${res.status}`);
+                return;
+            }
+
+            const blobRes = await fetch(`${apiUri}/image?id=${resJson['id']}`);
+            if (blobRes.status > 299) {
+                await interaction.editReply(`Result is not OK: HTTP ${res.status}`);
+                return;
+            }
+            const blob = await blobRes.blob();
+            const arrayBuf = await blob.arrayBuffer();
+            const buffer = Buffer.from(arrayBuf);
+
+            const fields = [];
+            for (const element of resJson['abilities']) {
+                fields.push({ name: 'Ability', value: element.name })
+            }
+
+            const file = new AttachmentBuilder(buffer);
+            let embed = new EmbedBuilder()
+                .setTitle(resJson['name'])
+                .setDescription(resJson['description'])
+                .addFields(
+                    { name: 'Author', value: `${resJson['author']}` },
+                    { name: 'Full Description', value: `${resJson['full_desc']}` }
+                )
+                .setThumbnail(`attachment://file.jpg`)
+                .setTimestamp()
+                .setFooter({ text: `ID: ${resJson['id']}` })
+
+            embed = embed.addFields(fields)
+
+            await interaction.editReply({ content: 'Found one!', embeds: [embed], files: [file] });
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            await interaction.editReply(`There was an error in TWM!`);
+        }
+    }
+}
