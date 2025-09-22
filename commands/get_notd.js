76a05a2912601d09379d8f6f69adcae04042c966
ziolkoplays <201670758+ziolkoplayscode@users.noreply.github.com>
2025-09-22 19:10:32 -0400

    Niko of the day for the bot :3c

---
+++ commands/get_notd.js
@@ -0,0 +1,55 @@
+const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder } = require('discord.js');
+const apiUri = process.env.API_URI
+const Buffer = require('buffer').Buffer
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('getnotd')
+        .setDescription('Replies with the Niko of the Day!'),
+
+    async execute(interaction) {
+        await interaction.reply("Looking for the Niko..");
+        try {
+            const niko = interaction.options.getString("nikosona");
+            const res =  await fetch(`https://nikodex.net/api/data/random_notd`);
+            const resJson = await reslist.json();
+            if (res.status > 299) {
+                await interaction.editReply(`Result is not OK: HTTP ${res.status}`);
+                return;
+            }
+
+            const blobRes = await fetch(`${apiUri}/image?id=${resJson['id']}`);
+            if (blobRes.status > 299) {
+                await interaction.editReply(`Result is not OK: HTTP ${res.status}`);
+                return;
+            }
+            const blob = await blobRes.blob();
+            const arrayBuf = await blob.arrayBuffer();
+            const buffer = Buffer.from(arrayBuf);
+
+            const fields = [];
+            for (const element of resJson['abilities']) {
+                fields.push({ name: 'Ability', value: element.name })
+            }
+
+            const file = new AttachmentBuilder(buffer);
+            let embed = new EmbedBuilder()
+                .setTitle(resJson['name'])
+                .setDescription(resJson['description'])
+                .addFields(
+                    { name: 'Author', value: `${resJson['author']}` },
+                    { name: 'Full Description', value: `${resJson['full_desc']}` }
+                )
+                .setThumbnail(`attachment://file.jpg`)
+                .setTimestamp()
+                .setFooter({ text: `ID: ${resJson['id']}` })
+
+            embed = embed.addFields(fields)
+
+            await interaction.editReply({ content: 'Niko found!', embeds: [embed], files: [file] });
+        } catch (error) {
+            console.log(`Error: ${error}`);
+            await interaction.editReply(`There was an error in TWM!`);
+        }
+    }
+}
