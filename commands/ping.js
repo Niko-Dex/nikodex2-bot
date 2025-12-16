5fcf9319c59da0453a8fc4025f10e831d7866498
ziolkoplays <201670758+ziolkoplayscode@users.noreply.github.com>
2025-12-15 20:34:16 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,6 +6,6 @@ module.exports = {
         .setDescription('Replies with pong!'),
     async execute(interaction) {
         await interaction.reply("Pong!");
-        console.log("'/ping' at" + interaction.guild.id);
+        console.log("'/ping' at " + interaction.channel.name + "(" + interaction.channel.id + ")" + " in " + interaction.guild.name + "(" + interaction.guild.id);
     }
 }
