f6da33fdc5c75816a491cbbba11c7f813f40fafc
thefirey33 <mmethefirey@gmail.com>
2026-01-26 04:13:40 +0300

    added special niko audit, that allows the admins to preview the image of the Niko when edited.

--- endpoints/audit.js
+++ endpoints/audit.js
@@ -1,43 +1,76 @@
-const { EmbedBuilder } = require('discord.js')
+const {
+  EmbedBuilder,
+  AttachmentBuilder,
+  Client,
+  Colors,
+  ButtonBuilder,
+} = require("discord.js");
+const { text } = require("express");
 
 /**
  *
  * @param {import('express').Request} req
  * @param {import('express').Response} res
+ * @param {Client} client
  */
 
 async function audit(req, res, client) {
-    try {
-        let embed = new EmbedBuilder()
-        .setTitle(`${req.fields['title']}`)
-        .setDescription(`${req.fields['msg']}`)
+  try {
+    let embed = new EmbedBuilder()
+      .setTitle(`${req.fields["title"]}`)
+      .setDescription(`${req.fields["msg"]}`);
 
-        if (req.fields['fields[n]']) {
-            const n = Number.parseInt(req.fields['fields[n]']);
-            console.log(n);
-            for (let i = 0; i < n; i++) {
-                const arr = req.fields[`fields[${i}]`].split(';')
-                if (arr.length != 2) {
-                    console.log('Arr len is not 2, maybe a ; is missing?')
-                }
-                else {
-                    embed = embed.addFields({
-                        name: arr[0], value: arr[1]
-                    })
-                }
-            }
+    if (req.fields["fields[n]"]) {
+      const n = Number.parseInt(req.fields["fields[n]"]);
+      console.log(n);
+      for (let i = 0; i < n; i++) {
+        const arr = req.fields[`fields[${i}]`].split(";");
+        if (arr.length != 2) {
+          console.log("Arr len is not 2, maybe a ; is missing?");
+        } else {
+          embed = embed.addFields({
+            name: arr[0],
+            value: arr[1],
+          });
         }
-
-        client.channels.cache.get(process.env['SUBMISSIONS_CHANNEL'])
-        .send({ embeds: [embed]})
-
-        res.send('Successful audit!')
+      }
     }
-    catch (e) {
-        res.status(404).send(JSON.stringify({ detail: `Problem while auditing! ${e}` }))
+    const channel = client.channels.cache.get(
+      process.env["SUBMISSIONS_CHANNEL"],
+    );
+    const nikoId = req.fields["noikId"];
+    if (nikoId) {
+      try {
+        await fetch(`${process.env["API_URI"]}/image?id=${nikoId}`)
+          .then((response) => {
+            if (!response.ok) throw "The response is not OK";
+            return response.blob();
+          })
+          .then(async (blob) => {
+            const arrayBuf = await blob.arrayBuffer();
+            const buffer = Buffer.from(arrayBuf);
+            const attachment = new AttachmentBuilder(buffer);
+            embed.setThumbnail(`attachment://file.jpg`);
+            embed.setFooter({ text: `Niko ID: ${nikoId}` });
+            channel.send({ embeds: [embed], files: [attachment] });
+            res.send("Successful Niko Audit!");
+          });
+      } catch {
+        channel.setFooter({ text: "Couldn't retrieve niko image!" });
+        channel.send({ embeds: [embed] });
+        res.send("Audit OK, But Image failure.");
+      }
+    } else {
+      channel.send({ embeds: [embed] });
+      res.send("Successful audit!");
     }
+  } catch (e) {
+    res
+      .status(404)
+      .send(JSON.stringify({ detail: `Problem while auditing! ${e}` }));
+  }
 }
 
 module.exports = {
-    audit
-}
+  audit,
+};
