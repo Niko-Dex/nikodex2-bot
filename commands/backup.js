0b50cd836b57fa4b57cfc64cda072b64ad07f054
Rainwater <collinziolko@hotmail.com>
2026-02-10 21:17:31 -0500

    owo2

--- commands/backup.js
+++ commands/backup.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder, MessageFlags} = require('discord.js');
 const adminIdList = process.env.ADMIN_ID_LIST.split(';')
-const uri = process.env.MYSQL_URI.split(';')
-const user = process.env.MYSQL_USER.split(';')
-const pass = process.env.MYSQL_PASS.split(';')
+const uri = process.env.MYSQL_URI
+const user = process.env.MYSQL_USER
+const pass = process.env.MYSQL_PASS
 const mysqldump = require('mysqldump')
+const port = process.env.MYSQL_PORT
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -21,19 +22,24 @@ module.exports = {
             })
             return
         } else {
-            mysqldump({
-                connection: {
-                    host: uri,
-                    user: user,
-                    password: pass,
-                    database: 'nikodex',
-                },
-                dumpToFile: './dump.sql',
-            });
-            channel.send({
-                content: 'mySQL data backup:', // Optional message content
-                files: ['./dump.sql']
-            })
+            try {
+                mysqldump({
+                    connection: {
+                        host: uri,
+                        user: user,
+                        password: pass,
+                        database: 'nikodex',
+                        port: port
+                    },
+                    dumpToFile: './dump.sql',
+                });
+                await interaction.reply({
+                    content: 'mySQL data backup:', // Optional message content
+                    files: ['./dump.sql']
+                })
+            } catch(e) {
+                console.log(e)
+            }
         }
     }
 }
